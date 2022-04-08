d2f946c1d040b3932b47682319bb32e37e770ed4
bowlerman <oskar@goldhahn.info>
2022-04-08 22:01:12 +0200

    simulator

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,34 +1,74 @@
-use std::{any::Any, convert::Infallible, error::Error, fs::File, collections::HashMap, hash::Hash, fmt::{Debug, Display}};
+use std::{
+    collections::HashMap,
+    error::Error,
+    fmt::{Debug},
+    fs::File,
+};
 
 use dioxus::prelude::*;
+#[cfg(not(debug_assertions))]
 use league_client_connector::LeagueClientConnector;
-use reqwest::{Client, RequestBuilder, Response};
+#[cfg(not(debug_assertions))]
+use reqwest::{Client, RequestBuilder};
+use serde_json::{self, from_value, Value};
+use tokio::time::*;
 use tract_onnx::prelude::*;
-use serde_json::{self, Value, from_value};
 
-#[derive(Debug)]
-struct LobbyState {
-    your_champ: u16,
-    bench: Vec<u16>,
-    team_champs: [u16; 4]
+type Champ = u16;
+
+#[derive(Debug, Clone, Default)]
+struct ChampSelectState {
+    your_champ: Champ,
+    bench: Vec<Champ>,
+    team_champs: [Champ; 4],
 }
 
-type Model = SimplePlan<TypedFact, Box<dyn TypedOp>, Graph<TypedFact, Box<dyn TypedOp>>>;
+struct Model {
+    model: SimplePlan<TypedFact, Box<dyn TypedOp>, Graph<TypedFact, Box<dyn TypedOp>>>,
+    champ_dict: HashMap<u16, usize>,
+}
 
-fn get_win_rate(team: &[u16; 5], champ_dict: &HashMap<u16, usize>, model: &Model) -> Result<f32, Box<dyn Error>> {
-    let tot_champs = champ_dict.len();
-    let mut one_hot = vec![0_f32; tot_champs + 1];
-    for champ in team {
-        one_hot[champ_dict[champ]] = 1.;
+impl Model {
+    fn get_win_rate(&self, team: &[u16; 5]) -> Result<f32, Box<dyn Error>> {
+        let tot_champs = self.champ_dict.len();
+        let mut one_hot = vec![0_f32; tot_champs + 1];
+        for champ in team {
+            one_hot[self.champ_dict[champ]] = 1.;
+        }
+        let input = tract_ndarray::arr1(&one_hot).into_shape((1, tot_champs + 1))?;
+        let tensor_res = self
+            .model
+            .run(tvec![input.into()])?
+            .to_vec()
+            .pop()
+            .expect("Expecting model output");
+        let res: [f32; 2] = tensor_res.as_slice()?.try_into()?;
+        let sum = res[0] + res[1];
+        Ok(res[0] / sum)
     }
-    let input = tract_ndarray::arr1(&one_hot).into_shape((1, tot_champs + 1))?;
-    let tensor_res = model.run(tvec![input.into()])?.to_vec().pop().expect("Expecting model output");
-    let res: [f32; 2] = tensor_res.as_slice()?.try_into()?;
-    let sum = res[0] + res[1];
-    Ok(res[0]/sum)
 }
 
-fn map_champ_id_to_index(all_champs: &Vec<(String, u16)>) -> Result<HashMap<u16, usize>, Box<dyn Error>> {
+impl ChampSelectState {
+    fn choices(&self) -> Vec<u16> {
+        let mut ret = vec![self.your_champ];
+        ret.extend(&self.bench);
+        ret
+    }
+}
+
+#[test]
+fn test_choices() {
+    let cs = ChampSelectState {
+        your_champ: 12,
+        bench: vec![1, 51, 124, 12, 53],
+        team_champs: [123, 12, 3, 1],
+    };
+    assert_eq!(cs.choices().len(), cs.bench.len() + 1)
+}
+
+fn map_champ_id_to_index(
+    all_champs: &Vec<(String, u16)>,
+) -> Result<HashMap<u16, usize>, Box<dyn Error>> {
     let mut map = HashMap::new();
     for i in 0..all_champs.len() {
         map.insert(all_champs[i].1, i);
@@ -36,92 +76,241 @@ fn map_champ_id_to_index(all_champs: &Vec<(String, u16)>) -> Result<HashMap<u16,
     Ok(map)
 }
 
-fn make_lobby_request() -> Result<RequestBuilder, Box<dyn Error>> {
-    let lock_file = LeagueClientConnector::parse_lockfile()?;
-    let request = Client::builder().danger_accept_invalid_certs(true).build()?
-    .get(
-        format! {"{protocol}://{ip}:{port}/lol-champ-select/v1/session", ip = lock_file.address, port = lock_file.port, protocol = lock_file.protocol},
-    ).header("authorization", format!{"Basic {auth}", auth = lock_file.b64_auth});
-    Ok(request)
+#[cfg(debug_assertions)]
+mod api_mock {
+
+    use std::{sync::{Arc, Mutex}, convert::Infallible};
+
+    use serde_json::{Value, json};
+    use lazy_static::lazy_static;
+    use crate::ChampSelectState;
+
+    lazy_static! {
+        static ref CHAMP_SELECT_STATE: Mutex<ChampSelectState> = Mutex::new(Default::default());
+    }
+    const SUMMONER_ID: u64 = 123;
+    #[derive(Debug, Clone)]
+    pub struct RequestBuilder {pub addr: String}
+
+    pub struct Response {response: Value}
+
+    impl RequestBuilder {
+        pub async fn send(self) -> Result<Response, reqwest::Error> {
+            let response = match self.addr.as_str() {
+                "lol-champ-select/v1/session" => {
+                    let champ_select_state = CHAMP_SELECT_STATE.lock().unwrap();
+                    let mut team_champs: Vec<Value> = champ_select_state.team_champs.iter().map(|champ| json!({
+                        "championId": champ
+                    })).collect();
+                    team_champs.push(json!({
+                        "championId": champ_select_state.your_champ
+                    }));
+                    json!(
+                    {
+                        "benchChampionIds": champ_select_state.bench,
+                        "myTeam": team_champs
+                    }
+                )},
+                "lol-summoner/v1/current-summoner" => json!({"summonerId": SUMMONER_ID}),
+                _ => unimplemented!()
+            };
+            return Ok(Response{response})
+        }
+
+        pub fn try_clone(&self) -> Option<RequestBuilder> {
+            Some(self.clone())
+        }
+    }
+
+    impl Response {
+        pub async fn json(&self) -> Result<Value, Infallible> {
+            return Ok(self.response.clone())
+        }
+    }
 }
 
-async fn get_summoner_id() -> Result<u64, Box<dyn Error>> {
-    let lock_file = LeagueClientConnector::parse_lockfile()?;
-    let request = Client::builder().danger_accept_invalid_certs(true).build()?
-    .get(
-        format! {"{protocol}://{ip}:{port}/lol-summoner/v1/current-summoner", ip = lock_file.address, port = lock_file.port, protocol = lock_file.protocol},
-    ).header("authorization", format!{"Basic {auth}", auth = lock_file.b64_auth});
-    let result: Value = request.send().await?.json().await?;
-    let summoner_id = result.as_object().ok_or("Expecting object with summoner info")?["summonerId"].as_u64().ok_or("Expecting summoner Id")?;
+#[cfg(debug_assertions)]
+use api_mock::*;
+
+fn make_lol_client_api_request(addr: &str) -> Result<RequestBuilder, Box<dyn Error>> {
+    #[cfg(debug_assertions)]{ // During debug return mock for client api
+        Ok(RequestBuilder{addr: addr.to_owned()})
+    }
+    #[cfg(not(debug_assertions))]{
+        let lock_file = LeagueClientConnector::parse_lockfile()?;
+        let request = Client::builder().danger_accept_invalid_certs(true).build()?
+        .get(
+            format! {"{protocol}://{ip}:{port}/{addr}", ip = lock_file.address, port = lock_file.port, protocol = lock_file.protocol, addr = addr},
+        ).header("authorization", format!{"Basic {auth}", auth = lock_file.b64_auth});
+        Ok(request)
+    }
+}
+
+fn make_lobby_request() -> Result<RequestBuilder, Box<dyn Error>> {
+    make_lol_client_api_request("lol-champ-select/v1/session")
+}
+
+async fn try_get_summoner_id() -> Result<u64, Box<dyn Error>> {
+    let result: Value = make_lol_client_api_request("lol-summoner/v1/current-summoner")?.send().await?.json().await?;
+    let summoner_id = result
+        .as_object()
+        .ok_or("Expecting object with summoner info")?
+        .get("summonerId")
+        .ok_or("Expecting summonerId field")?
+        .as_u64()
+        .ok_or("Expecting summoner Id")?;
     Ok(summoner_id)
 }
 
-async fn get_lobby_state(request: &RequestBuilder, summoner_id: u64) -> Result<LobbyState, Box<dyn Error>> {
-    let response = request.try_clone().ok_or("Could not clone Request")?.send().await?;
-    let base_json : Value = response.json().await?;
-    let json = base_json.as_object().ok_or("Expecting object at top level")?;
-    let bench = from_value(json["benchChampionIds"].clone())?;
-    let mut team_champs = Vec::new();
-    let mut your_champ = 0;
-    for member_val in json["myTeam"].as_array().ok_or("Expecting list of team members")? {
-        let member = member_val.as_object().ok_or("Expecting team member object")?;
-        let champ_id = from_value(member["championId"].clone())?;
-        if member["summonerId"].as_u64().ok_or("Expecting summoner Id of team member")? == summoner_id {
-            your_champ = champ_id;
-        } else {
-            team_champs.push(champ_id);
+struct ChampSelectFetcher {
+    request: RequestBuilder,
+    summoner_id: u64
+}
+impl ChampSelectFetcher {
+    async fn get_champ_select_state(
+        &self,
+    ) -> Result<ChampSelectState, Box<dyn Error>>
+    where
+        dyn Error: 'static,
+    {
+        let response = self.request.try_clone().ok_or("Could not clone champ select api request")?.send().await?;
+        let base_json: Value = response.json().await?;
+        let json = base_json
+            .as_object()
+            .ok_or("Expecting object at top level")?;
+        let bench = from_value(json["benchChampionIds"].clone())?;
+        let mut team_champs = Vec::new();
+        let mut your_champ = 0;
+        for member_val in json["myTeam"]
+            .as_array()
+            .ok_or("Expecting list of team members")?
+        {
+            let member = member_val
+                .as_object()
+                .ok_or("Expecting team member object")?;
+            let champ_id = from_value(member["championId"].clone())?;
+            if member["summonerId"]
+                .as_u64()
+                .ok_or("Expecting summoner Id of team member")?
+                == self.summoner_id
+            {
+                your_champ = champ_id;
+            } else {
+                team_champs.push(champ_id);
+            }
         }
+        let team_champs = team_champs.as_slice().try_into()?;
+        Ok(ChampSelectState {
+            your_champ,
+            bench,
+            team_champs,
+        })
     }
-    let team_champs = team_champs.as_slice().try_into()?;
-    Ok(LobbyState{ your_champ, bench, team_champs })
 }
 
-fn get_model(tot_champs: usize) -> Result<Model, Box<dyn Error>> {
+fn get_model() -> Result<Model, Box<dyn Error>> {
+    let champs: &Vec<(String, u16)> =
+        &serde_json::from_reader(File::open("model-trainer/champs.json").unwrap()).unwrap();
+    let tot_champs = champs.len();
+    let champ_dict = map_champ_id_to_index(champs).unwrap();
     let model = tract_onnx::onnx()
         // load the model
         .model_for_path("./model-trainer/model.onnx")?
         // specify input type and shape
-        .with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec![1, tot_champs + 1]))?
+        .with_input_fact(
+            0,
+            InferenceFact::dt_shape(f32::datum_type(), tvec![1, tot_champs + 1]),
+        )?
         // optimize graph
         .into_optimized()?
         // make the model runnable and fix its inputs and outputs
         .into_runnable()?;
-    Ok(model)
+    Ok(Model { model, champ_dict })
 }
-
 
 #[tokio::main]
 async fn main() {
-    let champs: &Vec<(String, u16)> = &serde_json::from_reader(File::open("model-trainer/champs.json").unwrap()).unwrap();
-    let tot_champs = champs.len();
-    let champ_dict = &map_champ_id_to_index(champs).unwrap();
-    let model = &get_model(tot_champs).unwrap();
-    let summoner_id = get_summoner_id().await.unwrap();
-    let request = &make_lobby_request().unwrap();
-    println!{"{:?}", get_win_rate(&[22, 16, 79, 112, 32], champ_dict, model)}
-    loop {
-        let lobby = get_lobby_state(request, summoner_id).await.unwrap();
-        for champ in [lobby.your_champ].clone().into_iter().chain(lobby.bench.clone().into_iter()) {
-            let team: [u16; 5] = [champ].into_iter().chain(lobby.team_champs.clone().into_iter()).collect::<Vec<_>>().try_into().unwrap();
-            let win_rate = get_win_rate(&team, champ_dict, model).unwrap();
-            print!{"{win_rate:.4} "};
+    dioxus::desktop::launch(App);
+}
+
+async fn init_champ_select_fetcher() -> ChampSelectFetcher {
+    let mut wait = interval(Duration::from_millis(1000));
+    let summoner_id = loop {
+        if let (Ok(summoner_id), _) = tokio::join!(try_get_summoner_id(), wait.tick()) {
+            break summoner_id;
         }
-        println!();
+        try_get_summoner_id().await.unwrap();
+    };
+    let request = loop {
+        if let Ok(req) = make_lobby_request() {
+            break req;
+        }
+        make_lobby_request().unwrap();
+        wait.tick().await;
+    };
+    ChampSelectFetcher{request, summoner_id}
+}
+
+#[allow(non_snake_case)]
+fn App(cx: Scope) -> Element {
+    let champ_select_fetcher = use_future(&cx, (), |_| async {
+        init_champ_select_fetcher().await
+    }).value();
+    let fetcher = match champ_select_fetcher
+    {
+        Some(fetcher) => fetcher,
+        None => return cx.render(rsx!("Waiting for lol client")),
+    };
+    cx.render(rsx!(ChampSelect {fetcher: fetcher}))
+}
+
+#[derive(Props)]
+struct ChampSelectProps<'a> {
+    fetcher: &'a ChampSelectFetcher
+}
+
+enum LolClientState {
+    ClientOpenWithChampSelect,
+    ClientOpenWithoutChampSelect,
+    ClientClosed
+}
+
+#[allow(non_snake_case)]
+fn ChampSelect<'a>(cx: Scope<'a, ChampSelectProps<'a>>) -> Element {
+    let model = use_state(&cx, || get_model().unwrap()).current().as_ref(); //application is useless without valid model
+    let lol_client_state = use_state(&cx, || LolClientState::ClientOpenWithoutChampSelect);
+    cx.render(rsx!(""))
+}
+
+
+/*
+
+if let Some(id) = summoner_id
+{
+let lobby_state_fut = use_future(&cx, &summoner_id,|_| async move {
+    Some(get_champ_select_state(request, summoner_id?).await)
+});
+
+let new_lobby_state = lobby_state_fut.value().and_then(|a| a.as_ref());
+let win_rate_display = match new_lobby_state {
+    Some(Ok(lobby_state)) => {
+        rsx!(ul {
+        lobby_state.choices().iter().map(|&choice| {
+            let team: [u16; 5] = [choice].into_iter().chain(lobby_state.team_champs.clone().into_iter()).collect::<Vec<_>>().try_into().unwrap();
+            let win_rate = get_win_rate(&team, champ_dict, model).unwrap();
+            rsx!("Win rate with {choice}: {win_rate}\n")
+        })})
+    },
+    Some(Err(e)) => rsx!("{e}"),
+    None => rsx!("Waiting for champ select"),
+};
+cx.render(rsx!(
+    button {
+        onclick: move |_| lobby_state_fut.restart(),
+        "Refresh Champ Select"
     }
-    //dioxus::desktop::launch(app)
-}
-
-fn app(mut cx: Scope) -> Element {
-    let champs: &Vec<(String, u16)> = &serde_json::from_reader(File::open("model-trainer/champs.json").unwrap()).unwrap();
-    let tot_champs = champs.len();
-    let champ_dict = &map_champ_id_to_index(champs).unwrap();
-    let model = &get_model(tot_champs).unwrap();
-    let (count, set_count) = use_state(&mut cx, || 0);
-
-    cx.render(rsx! {
-        h1 { "Count: {count}" }
-        button { onclick: move |_| set_count(count+1), "+" }
-        button { onclick: move |_| set_count(count-1), "-" }
-    })
-}
-
+    win_rate_display
+))}
+else {
+    cx.render(rsx!("kek"))
+}*/
